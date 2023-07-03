1caecfb229666e962b5644f5aad13cab225a4db3
aman <amanbihal98@gmail.com>
2023-07-03 18:37:34 +0530

    start

---
+++ app/Http/Controllers/admin/adminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class adminController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+   return view('admin.dashboard');
+    }
+    
+}
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\admin\adminController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::prefix('admin')->group(function()
+{
+    Route::get('dashboard',adminController::class)->name('admin.dashboard');
+});
+ 

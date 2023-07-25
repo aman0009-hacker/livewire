b8c87c19cd11a2c005f226037423f96779abe1f0
aman0009-hacker <amanbihal98@gmail.com>
2023-07-25 23:12:18 +0530

    25-07-23

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::prefix('admin')->group(function()
     Route::get('dashboard',adminController::class)->name('admin.dashboard');
     Route::get('users',ListUsers::class)->name('admin.users');
    
+   
  
 
 });
